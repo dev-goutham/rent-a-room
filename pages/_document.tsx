eed990b17c1d31fc8cf249812e82adc3977c6ec0
dev-goutham <dev.gouthamram95@gmail.com>
2022-03-01 20:53:59 +0530

    added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,22 @@ class MyDocument extends Document<any> {
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <link rel="apple-touch-icon" href="/logo.png" key="apple" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/logo.png"
+            key="icon32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/logo.png"
+            key="icon16"
+          />
+          <title>Rent A Room</title>
         </Head>
         <body>
           <Main />
