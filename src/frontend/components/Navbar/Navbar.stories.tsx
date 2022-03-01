959400ad1b514903e4ce8e63cbb5778fe5867399
dev-goutham <dev.gouthamram95@gmail.com>
2022-03-01 20:33:22 +0530

    bug fix

--- src/frontend/components/Navbar/Navbar.stories.tsx
+++ src/frontend/components/Navbar/Navbar.stories.tsx
@@ -5,22 +5,11 @@ import Navbar from "."
 export default {
   title: "Components/Navbar",
   component: Navbar,
-  argTypes: {
-    isAuthenticated: {
-      type: "boolean",
-    },
-  },
 } as ComponentMeta<typeof Navbar>
 
 const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />
 
 export const Unauthenticated = Template.bind({})
 
-Unauthenticated.args = {
-  isAuthenticated: false,
-}
-
 export const Authenticated = Template.bind({})
-Authenticated.args = {
-  isAuthenticated: true,
-}
+Authenticated.args = {}
