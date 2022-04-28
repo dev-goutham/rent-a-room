4fc4bdb7730d35f3ef00f146780601f0fc962ae0
dev-goutham <dev.gouthamram95@gmail.com>
2022-04-28 18:15:06 +0530

    added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import Layout from "@frontend/components/Layout"
 import AuthProvider from "@frontend/store/auth/context"
 import type { AppProps } from "next/app"
 import "tailwindcss/tailwind.css"
+import NextNProgress from "nextjs-progressbar"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <NextNProgress color="#1890FF" height={6} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
